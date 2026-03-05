37e9f6e5e6337aa6e0c96d78faf039c84b55b912
EkaterinaChuyko <kylefoxmay@yandex.ru>
2026-03-05 20:38:12 +0300

    Revert "Разработан функционал приложения smart-home"

--- telemetry/analyzer/src/main/java/ru/yandex/practicum/repository/ActionRepository.java
+++
@@ -1,8 +0,0 @@
-package ru.yandex.practicum.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.yandex.practicum.entity.Action;
-
-public interface ActionRepository extends JpaRepository<Action, Long> {
-}
-
