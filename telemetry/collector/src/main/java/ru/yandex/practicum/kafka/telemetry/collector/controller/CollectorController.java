71a42c81b743ab25026c3c5ad5f4476a3200d3cc
EkaterinaChuyko <kylefoxmay@yandex.ru>
2026-03-08 20:17:15 +0300

    Доработана реализация gRPC

--- telemetry/collector/src/main/java/ru/yandex/practicum/kafka/telemetry/collector/controller/CollectorController.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/kafka/telemetry/collector/controller/CollectorController.java
@@ -6,6 +6,7 @@ import org.springframework.kafka.core.KafkaTemplate;
 import ru.yandex.practicum.kafka.telemetry.event.DeviceAddedEventAvro;
 import ru.yandex.practicum.kafka.telemetry.event.DeviceTypeAvro;
 import ru.yandex.practicum.kafka.telemetry.event.HubEventAvro;
+import ru.yandex.practicum.kafka.telemetry.event.SensorEventAvro;
 import telemetry.service.collector.CollectorGrpc;
 import telemetry.service.collector.CollectorResponse;
 import telemetry.service.collector.HubEvent;
@@ -16,9 +17,11 @@ import java.time.Instant;
 public class CollectorController extends CollectorGrpc.CollectorImplBase {
 
     private final KafkaTemplate<String, HubEventAvro> hubKafkaTemplate;
+    private final KafkaTemplate<String, SensorEventAvro> sensorKafkaTemplate;
 
-    public CollectorController(KafkaTemplate<String, HubEventAvro> hubKafkaTemplate) {
+    public CollectorController(KafkaTemplate<String, HubEventAvro> hubKafkaTemplate, KafkaTemplate<String, SensorEventAvro> sensorKafkaTemplate) {
         this.hubKafkaTemplate = hubKafkaTemplate;
+        this.sensorKafkaTemplate = sensorKafkaTemplate;
     }
 
     @Override
@@ -31,6 +34,21 @@ public class CollectorController extends CollectorGrpc.CollectorImplBase {
         hubKafkaTemplate.send("telemetry.hubs.v1", avroEvent.getHubId(), avroEvent);
 
         CollectorResponse response = CollectorResponse.newBuilder().setSuccess(true).setMessage("Hub event received and sent to Kafka").build();
+
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void collectSensorEvent(telemetry.service.collector.SensorEvent request, StreamObserver<CollectorResponse> responseObserver) {
+
+        ru.yandex.practicum.kafka.telemetry.event.MotionSensorAvro motionPayload = ru.yandex.practicum.kafka.telemetry.event.MotionSensorAvro.newBuilder().setLinkQuality(255).setMotion(true).setVoltage(3000).build();
+
+        SensorEventAvro avroEvent = SensorEventAvro.newBuilder().setId(request.getSensorId()).setHubId(request.getHubId()).setTimestamp(Instant.now()).setPayload(motionPayload).build();
+
+        sensorKafkaTemplate.send("telemetry.sensors.v1", avroEvent.getId(), avroEvent);
+
+        CollectorResponse response = CollectorResponse.newBuilder().setSuccess(true).setMessage("Sensor event received and sent to Kafka").build();
 
         responseObserver.onNext(response);
         responseObserver.onCompleted();
