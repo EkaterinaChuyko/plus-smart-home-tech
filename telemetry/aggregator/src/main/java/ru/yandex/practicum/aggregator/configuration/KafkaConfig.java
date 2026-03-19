ed3aa5055906eda258682f3ef26d9d9e6fa9a89b
EkaterinaChuyko <kylefoxmay@yandex.ru>
2026-03-19 20:09:35 +0300

    merge config-server and add configuration setup

--- telemetry/aggregator/src/main/java/ru/yandex/practicum/aggregator/configuration/KafkaConfig.java
+++ telemetry/aggregator/src/main/java/ru/yandex/practicum/aggregator/configuration/KafkaConfig.java
@@ -7,6 +7,7 @@ import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import ru.yandex.practicum.aggregator.deserializer.SensorEventDeserializer;
@@ -17,6 +18,7 @@ import ru.yandex.practicum.kafka.telemetry.event.SensorsSnapshotAvro;
 import java.util.Properties;
 
 @Configuration
+@ConfigurationProperties("aggregator.kafka")
 public class KafkaConfig {
 
     @Value("${kafka.bootstrap-servers}")
